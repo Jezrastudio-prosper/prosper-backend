09f7608d03f42aa043d6422db0f7a91b4c0d1922
kutologojm <kutologomashego@gmail.com>
2026-06-07 21:29:41 +0800

    Added basic viewsets for finances app

--- apps/finances/views/api.py
+++ apps/finances/views/api.py
@@ -24,3 +24,31 @@ Example:
                 return Response({"detail": str(e)}, status=400)
             return Response(PostOutputSerializer(post).data, status=201)
 """
+
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from core.mixins import MultiSerializerMixin
+from apps.finances.models import Currency, Bank, Account
+from apps.finances.serializers import output as output_serializers
+from apps.finances.serializers import input as input_serializers
+
+
+class AccountViewSet(MultiSerializerMixin, ModelViewSet):
+    queryset = Account.objects.all()
+    input_serializer_class = input_serializers.AccountSerializer
+    output_serializer_class = output_serializers.AccountSerializer
+    lookup_field = "uuid"
+    lookup_url_kwarg = "uuid"
+
+
+class BankViewSet(ReadOnlyModelViewSet):
+    queryset = Bank.objects.all()
+    serializer_class = output_serializers.BankSerializer
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
+
+
+class CurrencyViewSet(ReadOnlyModelViewSet):
+    queryset = Currency.objects.all()  # todo make into a selector
+    serializer_class = output_serializers.CurrencySerializer
+    lookup_field = 'iso_code'
+    lookup_url_kwarg = 'iso_code'
